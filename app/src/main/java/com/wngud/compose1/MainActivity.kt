4f31f9471fe48c24c615717c7ed3a147d705d8d9
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-12-10 23:47:00 +0900

    [feat] Compose 사분면 문제 구현

--- app/src/main/java/com/wngud/compose1/MainActivity.kt
+++ app/src/main/java/com/wngud/compose1/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,9 +18,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +38,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     // problem1()
-                    problem2()
+                    // problem2()
+                    problem3()
                 }
             }
         }
@@ -91,10 +92,65 @@ fun problem2(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun problem3() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            composeCard(
+                text1 = "Text composable",
+                text2 = "Displays text and follows the recommended Material Design guidelines.",
+                bgColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            composeCard(
+                text1 = "Image composable",
+                text2 = "Creates a composable that lays out and draws a given Painter class object.",
+                bgColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            composeCard(
+                text1 = "Row composable",
+                text2 = "A layout composable that places its children in a horizontal sequence.",
+                bgColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            composeCard(
+                text1 = "Column composable",
+                text2 = "A layout composable that places its children in a vertical sequence.",
+                bgColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+@Composable
+fun composeCard(bgColor: Color, text1: String, text2: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(bgColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = text1,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = text2,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose1Theme {
-        problem2()
+        problem3()
     }
 }
