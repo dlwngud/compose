b1e334ae95f9b48611f50644edfa18f928274bb8
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-12-12 01:19:58 +0900

    [feat] Kotlin 기초 특별 경매

--- app/src/main/java/com/wngud/compose1/kotlin.kt
+++ app/src/main/java/com/wngud/compose1/kotlin.kt
@@ -1,42 +1,15 @@
 package com.wngud.compose1
 
-open class Phone(var isScreenLightOn: Boolean = false) {
-    open fun switchOn() {
-        isScreenLightOn = true
-    }
-
-    fun switchOff() {
-        isScreenLightOn = false
-    }
-
-    fun checkPhoneScreenLight() {
-        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
-        println("The phone screen's light is $phoneScreenLight.")
-    }
-}
-
-class FoldablePhone(var isFolded: Boolean = true): Phone(){
-    override fun switchOn(){
-        if (!isFolded) {
-            isScreenLightOn = true
-        }
-    }
-
-    fun fold() {
-        isFolded = true
-    }
-
-    fun unfold() {
-        isFolded = false
-    }
-}
-
 fun main() {
-    val phone = FoldablePhone()
+    val winningBid = Bid(5000, "Private Collector")
 
-    phone.switchOn()
-    phone.checkPhoneScreenLight()
-    phone.unfold()
-    phone.switchOn()
-    phone.checkPhoneScreenLight()
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+    println("Item B is sold at ${auctionPrice(null, 3000)}.")
+}
+
+class Bid(val amount: Int, val bidder: String)
+
+fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
+    // Fill in the code.
+    return bid?.amount?.coerceAtLeast(minimumPrice) ?: minimumPrice
 }
