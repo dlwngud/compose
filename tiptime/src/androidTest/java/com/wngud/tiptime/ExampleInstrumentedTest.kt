6df991086949a1cecd11d32d6af2d0c41bc4d77e
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-12-17 00:17:59 +0900

    [feat] 계측 테스트 작성 UI 구성요소는 composeTestRule을 통해 노드로 액세스할 수 있다. 일반적인 방법은 onNodeWithText() 메서드를 사용하여 특정 텍스트가 포함된 노드에 액세스하는 것

--- tiptime/src/androidTest/java/com/wngud/tiptime/ExampleInstrumentedTest.kt
+++ tiptime/src/androidTest/java/com/wngud/tiptime/ExampleInstrumentedTest.kt
@@ -1,24 +1,38 @@
 package com.wngud.tiptime
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.wngud.tiptime.ui.theme.Compose1Theme
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import java.text.NumberFormat
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.wngud.tiptime", appContext.packageName)
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            Compose1Theme {
+                TipTimeLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage").performTextInput("20")
+
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
     }
 }
