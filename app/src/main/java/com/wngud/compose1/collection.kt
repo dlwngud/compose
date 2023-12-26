c85c44baced2d260daa1657913dcb4930ac9a959
이주형 <70958782+dlwngud@users.noreply.github.com>
2023-12-26 22:54:23 +0900

    [feat] 작업2

--- app/src/main/java/com/wngud/compose1/collection.kt
+++ app/src/main/java/com/wngud/compose1/collection.kt
@@ -3,6 +3,12 @@ package com.wngud.compose1
 data class Event(
     val title: String,
     val description: String? = null,
-    val daypart: String,
+    val daypart: Daypart,
     val durationInMinutes: Int
 )
+
+enum class Daypart{
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
