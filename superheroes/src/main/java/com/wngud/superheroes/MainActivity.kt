e91255f331dd5a5405f6b24e486d76a109d9dd32
이주형 <70958782+dlwngud@users.noreply.github.com>
2024-01-09 23:38:18 +0900

    [feat] 연습:Superheroes 앱 빌드하기

--- superheroes/src/main/java/com/wngud/superheroes/MainActivity.kt
+++ superheroes/src/main/java/com/wngud/superheroes/MainActivity.kt
@@ -3,13 +3,25 @@ package com.wngud.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.wngud.superheroes.model.Hero
+import com.wngud.superheroes.model.heroList
 import com.wngud.superheroes.ui.theme.Compose1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +34,44 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SuperheroesApp()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SuperheroesApp() {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar()
+            }
+        ) {
+            HeroesList(heroes = heroList, contentPadding = it)
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Compose1Theme {
-        Greeting("Android")
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TopAppBar(modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            },
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun SuperHeroesPreview() {
+        Compose1Theme {
+            SuperheroesApp()
+        }
     }
 }
--- superheroes/src/main/java/com/wngud/superheroes/model/Hero.kt
+++ superheroes/src/main/java/com/wngud/superheroes/model/Hero.kt
@@ -2,9 +2,19 @@ package com.wngud.superheroes.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.wngud.superheroes.R
 
 data class Hero(
     @StringRes val name: Int,
     @StringRes val content: Int,
     @DrawableRes val imageResourceId: Int,
+)
+
+val heroList = listOf<Hero>(
+    Hero(R.string.hero1, R.string.description1, R.drawable.android_superhero1),
+    Hero(R.string.hero2, R.string.description2, R.drawable.android_superhero2),
+    Hero(R.string.hero3, R.string.description3, R.drawable.android_superhero3),
+    Hero(R.string.hero4, R.string.description4, R.drawable.android_superhero4),
+    Hero(R.string.hero5, R.string.description5, R.drawable.android_superhero5),
+    Hero(R.string.hero6, R.string.description6, R.drawable.android_superhero6),
 )
